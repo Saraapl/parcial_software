6651ddf8674b19d47eb611360078db6607ab7262
Sara Pineda Lujan <Sariidemg@gmail.com>
2025-08-25 19:31:19 -0500

    commit

--- parcial01_SaraPineda/urls.py
+++ parcial01_SaraPineda/urls.py
@@ -1,22 +1,11 @@
-"""
-URL configuration for parcial01_SaraPineda project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from vuelos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('registrar/', views.registrar_vuelo, name="registrar_vuelo"),
+    path('listar/', views.listar_vuelos, name="listar_vuelos"),
+    path('estadisticas/', views.estadisticas, name="estadisticas"),
 ]
--- vuelos/views.py
+++ vuelos/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Flight
+from django.db.models import Avg
 
-# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+
+def registrar_vuelo(request):
+    if request.method == "POST":
+        nombre = request.POST.get("nombre")
+        tipo = request.POST.get("tipo")
+        precio = request.POST.get("precio")
+
+        if nombre and tipo and precio:
+            Flight.objects.create(
+                nombre=nombre,
+                tipo=tipo,
+                precio=precio
+            )
+            return redirect("listar_vuelos")
+    return render(request, "registrar.html")
+
+
+def listar_vuelos(request):
+    vuelos = Flight.objects.all().order_by("precio")
+    return render(request, "listar.html", {"vuelos": vuelos})
+
+
+def estadisticas(request):
+    total_nacionales = Flight.objects.filter(tipo="Nacional").count()
+    total_internacionales = Flight.objects.filter(tipo="Internacional").count()
+    promedio_nacional = Flight.objects.filter(tipo="Nacional").aggregate(promedio=Avg("precio"))["promedio"]
+
+    return render(request, "estadisticas.html"), {
+        "total_nacionales": total_nacionales,
+        "total_internacionales": total_internacionales,
+        "promedio_nacional": promedio_nacional
+
+    }
